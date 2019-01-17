069ffcb529215fbf276cc8f0e2f653cb868d545d
Matt Mower <m@mwrvld.com>
2019-01-17 23:18:48 +0000

    Initial Commit

---
+++ Moneo/Moneo.h
@@ -0,0 +1,19 @@
+//
+//  Moneo.h
+//  Moneo
+//
+//  Created by Matthew Mower on 17/01/2019.
+//  Copyright © 2019 TAON. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Moneo.
+FOUNDATION_EXPORT double MoneoVersionNumber;
+
+//! Project version string for Moneo.
+FOUNDATION_EXPORT const unsigned char MoneoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Moneo/PublicHeader.h>
+
+
